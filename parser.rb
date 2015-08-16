7aa33db6606e310b0a1339731f61d7e7ac3689f7
Arjun Kavi <arjun.kavi@gmail.com>
2015-08-16 16:19:00 -0700

    Add functional lexing
    Seems to be working for a simple test case

--- parser.rb
+++ parser.rb
@@ -1,4 +1,6 @@
 class Lexer
+  Token = Struct.new(:type, :value)
+
   TOKENS = {
     ']->' => 'CLOSE_PARAMS',
     ']' => 'CLOSE_INVOKE',
@@ -15,58 +17,83 @@ class Lexer
     ')' => 'CLOSE_PAREN',
     ',' => 'SEPARATOR',
     "\n" => 'END_LINE',
+    ";" => 'END_EXPRESSION',
     /".*?"/ => 'STRING',
     "true" => 'TRUE',
     "false" => 'FALSE',
     "if" => 'IF',
-    /[a-zA-Z]/ => 'REF',
-    /[0-9]/ => 'NUM',
+    /[a-zA-Z]*/ => 'REF',
+    /[0-9]*/ => 'NUM',
   }
+
+  def lex(string)
+    tokens = []
+    while !string.empty?
+      string = string.slice(1..-1) while string.index(/[^\S\n]/) == 0
+      TOKENS.each do |matcher, type|
+        matcher = Regexp.new(Regexp.quote(matcher)) if matcher.is_a?(String)
+        if string.index(matcher) == 0
+          match = string.match(matcher)[0]
+          token = Token.new(type, match)
+          tokens << token
+          string = string.slice(match.length..-1)
+          break
+        end
+      end
+    end
+
+    tokens
+  end
 end
 
 class Parser
   GRAMMAR = {
-    'block' => 'root'
+    'block' => 'root',
 
-    'expression END_LINE' => 'block'
-    'block expression END_LINE' => 'block'
+    'expression ender' => 'block',
+    'block expression ender' => 'block',
+    'END_LINE' => 'ender',
+    'END_EXPRESSION' => 'ender',
 
-    'if_block' => 'expression'
-    'function' => 'expression'
-    'invocation' => 'expression'
-    'assignment' => 'expression'
-    'variable' => 'expression'
-    'literal' => 'expression'
-    'OPEN_PAREN expression CLOSE_PAREN' => 'expression'
-    'expression EQUALS expression' => 'expression'
-    'NEGATE expression' => 'expression'
+    'if_block' => 'expression',
+    'function' => 'expression',
+    'invocation' => 'expression',
+    'assignment' => 'expression',
+    'variable' => 'expression',
+    'literal' => 'expression',
+    'OPEN_PAREN expression CLOSE_PAREN' => 'expression',
+    'expression EQUALS expression' => 'expression',
+    'NEGATE expression' => 'expression',
 
-    'params OPEN_BLOCK block CLOSE_BLOCK' => 'function'
-    'OPEN_PARAMS CLOSE_PARAMS' => 'params'
-    'OPEN_PARAMS variable CLOSE_PARAMS' => 'params'
-    'OPEN_PARAMS param_list CLOSE_PARAMS' => 'params'
-    'variable SEPARATOR variable' => 'param_list'
+    'params OPEN_BLOCK block CLOSE_BLOCK' => 'function',
+    'OPEN_PARAMS CLOSE_PARAMS' => 'params',
+    'OPEN_PARAMS variable CLOSE_PARAMS' => 'params',
+    'OPEN_PARAMS param_list CLOSE_PARAMS' => 'params',
+    'variable SEPARATOR variable' => 'param_list',
 
-    'expression arguments' => 'invocation'
-    'OPEN_INVOKE CLOSE_INVOKE' => 'arguments'
-    'OPEN_INVOKE expression CLOSE_INVOKE' => 'arguments'
-    'OPEN_INVOKE expression SEPARATOR expression CLOSE_INVOKE' => 'arguments'
+    'expression arguments' => 'invocation',
+    'OPEN_INVOKE CLOSE_INVOKE' => 'arguments',
+    'OPEN_INVOKE expression CLOSE_INVOKE' => 'arguments',
+    'OPEN_INVOKE expression SEPARATOR expression CLOSE_INVOKE' => 'arguments',
 
-    'IF expression OPEN_BLOCK block CLOSE_BLOCK' => 'if_block'
+    'IF expression OPEN_BLOCK block CLOSE_BLOCK' => 'if_block',
 
-    'variable ASSIGN expression' => 'assignment'
+    'variable ASSIGN expression' => 'assignment',
 
-    'REF' => 'variable'
-    'ROOT_SIGIL REF' => 'variable'
-    'PARENT_SIGIL REF' => 'variable'
-    'expression LOCAL_SIGIL REF' => 'variable'
+    'REF' => 'variable',
+    'ROOT_SIGIL REF' => 'variable',
+    'PARENT_SIGIL REF' => 'variable',
+    'expression LOCAL_SIGIL REF' => 'variable',
 
-    'NUM' => 'literal'
-    'TRUE' => 'literal'
-    'FALSE' => 'literal'
-    'STRING' => 'literal'
+    'NUM' => 'literal',
+    'TRUE' => 'literal',
+    'FALSE' => 'literal',
+    'STRING' => 'literal',
   }
+
+  def parse
+  end
 end
 
-class Runner
-end
+tokens = Lexer.new.lex("[]->{foo}")
+puts "TOKENS: #{tokens.inspect}"
